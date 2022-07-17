59dee602bbf03e45d170deb967a05afd9a166aa3
FelixJ27 <1045985453@qq.com>
2022-07-17 22:17:34 +0800

    快速乘

---
+++ src/com/leetcode/util/QuickCal.java
@@ -0,0 +1,33 @@
+package com.leetcode.util;
+
+/**
+ * @Author: Felix
+ * @Description:
+ */
+public class QuickCal {
+
+    public boolean quickMul(int y, int z, int x) {
+        // x 和 y 是负数，z 是正数
+        // 需要判断 z * y >= x 是否成立
+        int result = 0, add = y;
+        while (z != 0) {
+            if ((z & 1) != 0) {
+                // 需要保证 result + add >= x
+                if (result < x - add) {
+                    return false;
+                }
+                result += add;
+            }
+            if (z != 1) {
+                // 需要保证 add + add >= x
+                if (add < x - add) {
+                    return false;
+                }
+                add += add;
+            }
+            // 不能使用除法
+            z >>= 1;
+        }
+        return true;
+    }
+}
